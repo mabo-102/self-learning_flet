f73ecdfed13b3f0a16afab9cb1d857e767572aa6
mabo-102 <mabo102.dev@gmail.com>
2023-10-12 14:42:02 +0900

    Append: Filtering list items. #6

--- todo.py
+++ todo.py
@@ -1,13 +1,17 @@
 import flet as ft
 
 class Task(ft.UserControl):
-    def __init__(self, task_name, task_delete):
+    def __init__(self, task_name, task_status_change, task_delete):
         super().__init__()
+        self.completed = False
         self.task_name = task_name
+        self.task_status_change = task_status_change
         self.task_delete = task_delete
 
     def build(self):
-        self.display_task = ft.Checkbox(value=False, label=self.task_name)
+        self.display_task = ft.Checkbox(
+            value=False, label=self.task_name, on_change=self.status_changed
+        )
         self.edit_name = ft.TextField(expand=1)
 
         self.display_view = ft.Row(
@@ -63,12 +67,22 @@ class Task(ft.UserControl):
 
     def delete_clicked(self, e):
         self.task_delete(self)
+    
+    def status_changed(self, e):
+        self.completed = self.display_task.value
+        self.task_status_change(self)
 
 
 class TodoApp(ft.UserControl):
     def build(self):
         self.new_task = ft.TextField(hint_text="Whats needs to be done?", expand=True)
         self.tasks = ft.Column()
+
+        self.filter = ft.Tabs(
+            selected_index=0,
+            on_change=self.tabs_changed,
+            tabs=[ft.Tab(text="all"), ft.Tab(text="active"), ft.Tab(text="completed")], 
+        )
 
         return ft.Column(
             width = 600,
@@ -79,19 +93,41 @@ class TodoApp(ft.UserControl):
                         ft.FloatingActionButton(icon=ft.icons.ADD, on_click=self.add_clicked),
                     ],
                 ),
-                self.tasks,
+                ft.Column(
+                    spacing=25,
+                    controls=[
+                        self.filter,
+                        self.tasks,
+                    ],
+                ),
             ],
         )
 
+    def update(self):
+        status = self.filter.tabs[self.filter.selected_index].text
+        for task in self.tasks.controls:
+            task.visible = (
+                status == "all"
+                or (status == "active" and task.completed == False)
+                or (status == "completed" and task.completed)
+            )
+        super().update()
+
 
     def add_clicked(self, e):
-        task = Task(self.new_task.value, self.task_delete)
+        task = Task(self.new_task.value, self.task_status_change, self.task_delete)
         self.tasks.controls.append(task)
         self.new_task.value = ""
         self.update()
 
+    def task_status_change(self, e):
+        self.update()
+
     def task_delete(self, task):
         self.tasks.controls.remove(task)
+        self.update()
+
+    def tabs_changed(self, e):
         self.update()
 
 
